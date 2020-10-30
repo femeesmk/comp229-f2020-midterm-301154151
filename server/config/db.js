b8bc2d9aa52a9d22d618bf4711dc7a6956123ba1
Femees <femeesmk@gamil.com>
2020-10-30 14:47:39 -0400

    fixed add button, added edit and delete button,connected to remote mongodb, package.json update

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //Connecting to remote Mongodb database->
+  "URI": "mongodb+srv://midterm-301154151-user:lXPVrusKKl4HN8dc@midterm-301154151.rcf9e.mongodb.net/midterm-301154151?retryWrites=true&w=majority"
 };
